8f0b97a369ce229184cf39d556c47486921acef9
amphineko <sukareki@gmail.com>
2019-08-23 04:10:52 +0800

    fix missing server config

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ async function fetchServerDocument(url: string) {
     return new DOMParser().parseFromString(await response.text())
 }
 
-async function getServers(url: UrlEntry, slice: number) {
+async function getServers(url: UrlEntry, slice: number): Promise<[Array<Element>, Element]> {
     const document = await fetchServerDocument(url.url)
     const root = document.getElementsByTagName('servers')[0]
     const servers = root.getElementsByTagName('server')
@@ -21,20 +21,31 @@ async function getServers(url: UrlEntry, slice: number) {
         name.textContent = `${name.textContent} ${url.suffix}`
         result.push(server)
     }
-    return result
+    return [result, root.getElementsByTagName('config')[0]]
 }
 
-async function collectServers(pending: Array<Element>) {
+async function collectServers(pending: Array<Element>, config: Element) {
     const root = new DOMImplementation().createDocument(null, null, null)
     const servers = root.createElement('servers')
     root.appendChild(servers)
     pending.forEach(server => servers.appendChild(server))
+    servers.appendChild(config)
     return root
 }
 
 export async function generate(urls: Array<UrlEntry>, slice: number) {
-    const serverArray = await Promise.all(urls.map(url => getServers(url, slice)))
-    const document = await collectServers(serverArray.flat())
+    let appendConfig: Element | undefined
+    const serverArray = await Promise.all(urls.map(url => {
+        return getServers(url, slice).then(([result, config]) => {
+            if (!appendConfig)
+                appendConfig = config
+            return result
+        })
+    }))
+
+    if (!appendConfig)
+        throw 'Server config is undefined'
+    const document = await collectServers(serverArray.flat(), appendConfig)
 
     return new XMLSerializer().serializeToString(document)
 }
